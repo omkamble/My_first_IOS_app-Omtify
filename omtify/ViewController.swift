924bc4f238bb196d9d1914e95b228d7d7c065b83
omkamble <omk10902@gmail.com>
2022-12-15 13:31:10 +0530

    Initial Commit

---
+++ omtify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  omtify
+//
+//  Created by OM KAMBLE on 15/12/22.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
